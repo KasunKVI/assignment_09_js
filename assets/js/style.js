91120519f2002749444dc306c129997725b663e9
KasunKVI <kkhettiarachchi10@gmail.com>
2023-09-26 09:41:54 +0530

    complete the assignment

--- assets/js/style.js
+++ assets/js/style.js
@@ -5,22 +5,36 @@ let lightCount = 7;
 let intervalId = null;
 
 var lightOn = {
-    background: "#d11f1f",
-    boxShadow:  "20px 20px 60px #b21a1a,20px -20px 60px #f02424"
+    background: "#d11f1f", boxShadow:  "20px 20px 60px #b21a1a , 20px -20px 60px #f02424"
+
 }
+
+var lightOff = {
+    background: "white", boxShadow:  "black"
+
+}
+var lightOn1 = {
+    background: "#fc7b7b", boxShadow:  "20px 20px 60px #d66969 , -20px -20px 60px #ff8d8d"
+
+}
+var lightOn2 = {
+    background: "#ffbdbd", boxShadow:  "20px 20px 60px #d9a1a1,-20px -20px 60px #ffd9d9"
+
+}
+
 setLight = () =>{
 
-    $("div").css("background", "white");
+    $("div").css(lightOff);
 
     if (currentDirection==="Right") {
 
         currentLight++
 
-        $("div").eq(currentLight).css("background", "red");
+        $("div").eq(currentLight).css(lightOn);
 
         if (currentLight - 1 > 0) {
-            $("div").eq(currentLight - 1).css("background", "#fc7b7b" , "box-shadow", "20px 20px 60px #d66969 -20px -20px 60px #ff8d8d");
-            $("div").eq(currentLight - 2).css("background", "#ffbdbd");
+            $("div").eq(currentLight - 1).css(lightOn1);
+            $("div").eq(currentLight - 2).css(lightOn2);
         }
 
         if (currentLight===(lightCount-1)){
@@ -28,9 +42,9 @@ setLight = () =>{
         }
     }else {
         currentLight--
-        $("div").eq(currentLight).css("background", "red");
-        $("div").eq(currentLight+1).css("background", "#fc7b7b");
-        $("div").eq(currentLight+2).css("background", "#ffbdbd")
+        $("div").eq(currentLight).css(lightOn);
+        $("div").eq(currentLight+1).css(lightOn1);
+        $("div").eq(currentLight+2).css(lightOn2)
 
         if (currentLight ===0){
             currentDirection="Right";
@@ -50,7 +64,7 @@ $("#start").on('click', () => {
 
 $("#stop").on('click', () => {
     clearInterval(intervalId);
-    intervalId==null;
+    intervalId=null;
     $("#audio")[0].pause();
 });
 
